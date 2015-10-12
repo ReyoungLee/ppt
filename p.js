5bc55733d29660c3c9da6b71292b534cad4802bf
reyounglee <reyounglee@live.cn>
2015-10-12 23:16:35 +0800

    almost done

--- p.js
+++ p.js
@@ -1,3 +1,13 @@
+
+Object.prototype.css = function (obj) {
+	var _t = this.length == undefined ? [this]:this,i,k;
+	for(i = _t.length;i--;){
+		for(k in obj){
+			_t[i].style[k] = obj[k];
+		}
+	}
+}
+
 var ppt = {
 	pages: document.querySelectorAll('#con>div'),
 	l: 0,
@@ -8,14 +18,22 @@ var ppt = {
 			return;
 		}
 
-		var cp = this.pages[this.currentPage], np = this.pages[index];
+		var dn = {transform: 'perspective(999px) rotateX(-90deg) rotateY(62deg)'},
+			up = {transform: 'perspective(999px) rotate(0)', opacity: 1};
 
-		index > this.currentPage?
-			(cp.style.transform = 'perspective(999px) rotateX(-90deg)'):
-			(np.style.transform = 'perspective(999px) rotateX(0)');
+		if( index > this.currentPage){
+			var target = this.pages.slice(this.currentPage, index);
+			target.css(dn);
+			setTimeout(function(){
+				target.css({opacity: 0});
+			},300);
+		}else{
+			this.pages.slice(index, this.currentPage).css(up);
+		}
 
 		this.nav.style.width = (index+1)/this.l * 100 + '%';
 		this.currentPage = index;
+		location.hash = index;
 	},
 	goUp: function () {
 		this.go(this.currentPage - 1);
@@ -35,16 +53,24 @@ var ppt = {
 		([8,33,37,38].indexOf(e) > -1) && ppt.goUp();
 		([32,34,39,40].indexOf(e) > -1) && ppt.goDn();
 	},
-	init: function () {
-		this.l = this.pages.length;
-		var i;
-		for(i = this.l; i--;){
-			this.pages[i].style.background = this.color();
-			this.pages[i].style.zIndex = this.l - i;
+	init: function (_t) {
+		_t.pages.slice = Array.prototype.slice;
+		_t.l = _t.pages.length;
+		var i, p = _t.getNav();
+		for(i = _t.l; i--;){
+			_t.pages[i].css({background: _t.color()});
+			_t.pages[i].css({zIndex: _t.l - i});
 		}
-		this.go(0);
-		document.onkeydown = this.keyEvent;
+		_t.go(0);
+		setTimeout(function () {
+			_t.go(p);
+		},0);
+		document.onkeydown = _t.keyEvent;
+	},
+	getNav: function () {
+		var i = location.hash.slice(-1) * 1;
+		typeof i == 'number' || (i = 0);
+		return i;
 	}
 }
-
-ppt.init();
+ppt.init(ppt);
