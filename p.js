2b4dd7289bc2b6efdd7be2dd35dce79eaa89abb4
reyounglee <reyounglee@live.cn>
2015-11-12 13:40:55 +0800

    show img&hash optim

--- p.js
+++ p.js
@@ -5,22 +5,23 @@
 		l: 0,
 		nav: d.querySelector('#nav>div'),
 		tsn: ' cubic-bezier(0, 0, 0.7, 1.7)',
+		ts: {transition: ''},
+		tf: {transform: ''},
 		currentPage: 0,
 		events: [],
 		q: function (c) {
 			return d.querySelectorAll(c);
 		},
-		go: function (index) {
+		_go: function (index) {
 			var _t = this;
-			if(index >= _t.l || index < 0){
-				return;
-			}
+			index >= _t.l && (index = _t.l - 1);
+			index < 0 && (index = 0);
 
 			var dn = {transform: 'perspective(999px) rotateX(-90deg) rotateY(62deg)', opacity: 0},
 				up = {transform: 'perspective(999px) rotate(0)', opacity: 1};
 
 			if( index > _t.currentPage){
-				_t.pages.slice(_t.currentPage, index).css(dn);//css({opacity: 0},300);
+				_t.pages.slice(_t.currentPage, index).css(dn);//.css({opacity: 0},300);
 			}else{
 				_t.pages.slice(index, _t.currentPage).css(up);
 			}
@@ -29,11 +30,12 @@
 			_t.currentPage = location.hash = index;
 			_t.events[index] && _t.events[index]();
 		},
-		goUp: function () {
-			this.go(this.currentPage - 1);
-		},
-		goDn: function () {
-			this.go(this.currentPage + 1);
+		changeHash: function (d) {
+			var targetIndex = this.getNav() + d;
+			if(targetIndex >= this.l || targetIndex < 0){
+				return;
+			}
+			location.hash = targetIndex;
 		},
 		color:  function (flag) {
 			var ret,c = this.color;
@@ -44,12 +46,15 @@
 		keyEvent: function (e) {
 			e.which == 8 && e.preventDefault();
 			var e = e.which;
-			([8,33,37,38].indexOf(e) > -1) && ppt.goUp();
-			([32,34,39,40].indexOf(e) > -1) && ppt.goDn();
+			([8,33,37,38].indexOf(e) > -1) && ppt.changeHash(-1);
+			([32,34,39,40].indexOf(e) > -1) && ppt.changeHash(1);
 		},
 		getNav: function () {
-			var i = location.hash.slice(-1) * 1;
-			typeof i == 'number' || (i = 0);
+			var i = location.hash.slice(1) * 1;
+			if(typeof i != 'number' || i == NaN){
+				i = 0;
+				location.hash = 0;
+			}
 			return i;
 		},
 		init: function (_t) {
@@ -59,19 +64,18 @@
 			_t.initLine(_t);
 			_t.initPie(_t);
 			_t.initBar(_t);
+			_t.initSC(_t);
 
 			var go = function () {
-				_t.go(_t.getNav());
+				_t._go(_t.getNav());
 			}
 			setTimeout(go,0);
-			// setTimeout(function () {
-			// 	_t.q('img').css({opacity: 1});
-			// }, 111)
 			document.onkeydown = _t.keyEvent;
 			window.onhashchange = go;
 		},
 		initPage: function (_t) {
 
+			_t.q('#page').css({opacity: 1, transition: 'opacity .4s ease-out'},400);
 			_t.l = _t.pages.length;
 			var i;
 
@@ -91,7 +95,7 @@
 			var bg = _t.pages[1].style.background;
 
 			_t.events[1] = function () {
-				[spot, txt].css({left: 0, transition: ''});
+				[spot, txt].css(_t.ts).css({left: 0});
 				txte.css({bottom: 0});
 				txto.css({top: 0});
 				setTimeout(function () {
@@ -160,7 +164,7 @@
 				prj = _t.q('#works .prj'),
 				axis = _t.q('#works .axis'),
 				start = _t.q('#works .start');
-			var wk = [2,3,2,2,4,5],
+			var wk = [2,3,2,2,3.2,5],
 				colors = ['#AAF6FF', '#65E2F1', '#3EBFCE', '#0A9AAB', '#027B8C', '#065C69'],
 				i;
 			var l = wk.length;
@@ -170,21 +174,43 @@
 			}
 
 			_t.events[3] = function () {
-
-				[bars, axis, start, prj].css({transition: ''});
+				[bars, axis, start, prj].css(_t.ts);
 				bars.css({height: 0});
 				axis.css({width: 0});
 				start.css({opacity: 0});
 				prj.css({transform: '', opacity: 0});
 				
 				setTimeout(function () {
-					axis.css({transition: '1.5s ease-out', width: '100%'});
+					axis.css({transition: '1.8s ease-out', width: '100%'});
 					start.css({transition: '3s', opacity: 1}, 200);
 					for(i = 0;i < l;i++){
 						bars[i].css({transition: '.3s' + _t.tsn, height: '100%'}, 300 * i);
-						prj[i].css({transition: '.3s ease-out', transform: 'rotate(40deg)', opacity: 1}, 300 * i)
+						prj[i].css({transition: '.3s ease-out', transform: 'rotate(40deg)', opacity: 1}, 300 * i);
 					}
 				},99);
+			}
+		},
+		initSC: function (_t) {
+			var imgs = _t.q('.pc img'),
+				imgc = _t.q('.pc');
+			for(var i = imgc.length;i--;){
+				(function () {
+					var idx = _t.pages.indexOf(imgc[i]);
+					_t.events[idx] = function () {
+						var picL = _t.pages[idx].querySelectorAll('.left'),
+							picR = _t.pages[idx].querySelectorAll('.right'),
+							pic = _t.pages[idx].querySelectorAll('img');
+
+						pic.css(_t.ts).css({opacity: 0});
+						picL.css({transform: 'translate(20vw) scale(0.5)'});
+						picR.css({transform: 'translate(-30vw) scale(0.5)'});
+						setTimeout(function () {
+							pic.css({transition: '.4s ease-out', opacity: 1});
+							picL.css(_t.tf);
+							picR.css(_t.tf);
+						},199);
+					}
+				})();
 			}
 		},
 		initFuns: function () {
